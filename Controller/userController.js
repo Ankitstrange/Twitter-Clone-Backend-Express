b6c70464612e8aa90edaa9c2e9f527b2463bd6dc
Ankitstrange <ankitbhardwaj4903@gmail.com>
2022-09-24 17:03:29 +0530

    created get user and delete user services

--- Controller/userController.js
+++ Controller/userController.js
@@ -19,19 +19,19 @@ exports.getAllUsers = async(req,res,next)=>{
 exports.addUser = async(req,res,next)=>{
     try{
         const userByScreenName = await Users.find({screenName:req.body.screenName});
-        if(userByScreenName||userByScreenName.length>0){
+        if(userByScreenName.length>0){
             return res.status(404).json({
                 message:"User allready exists with the given username. Please choose other username!"
             });
         }
         const userByEmail = await Users.find({email:req.body.email});
-        if(userByEmail||userByEmail.length>0){
+        if(userByEmail.length>0){
             return res.status(404).json({
                 message:"Email allready in use. Please enter different email!"
             });
         }
         const body = {};
-        body.userId = userIdGenerator();
+        body.userId = await userIdGenerator();
         body.screenName=String(req.body.screenName).trim();
         body.name = String(req.body.name).trim();
         body.description = req.body.description?String(req.body.description).trim():undefined;
@@ -41,6 +41,7 @@ exports.addUser = async(req,res,next)=>{
         body.followersCount = req.body.followersCount?req.body.followersCount:undefined;
         body.tweetsCount = req.body.tweetsCount?req.body.tweetsCount:undefined;
         body.likesCount = req.body.likesCount?req.body.likesCount:undefined;
+        body.email = String(req.body.email).trim();
         const user = await Users.create(body);
         if(!user){
             res.status(500).json({
@@ -50,6 +51,38 @@ exports.addUser = async(req,res,next)=>{
             res.status(200).json(user);
         }
     }catch(exception){
+        next(exception);
+    }
+}
+
+exports.getUser = async(req,res,next)=>{
+    try{
+        const user = await Users.find({userId:req.params.id});
+        if(user.length===0){
+            res.status(404).json({
+                message:"User not found"
+            });
+        } else {
+            res.status(200).json(user);
+        }
+    } catch (exception) {
+        next(exception);
+    }
+}
+
+exports.deleteUser = async(req, res, next)=>{
+    try{
+        const deletedUser = await Users.deleteOne({userId:req.params.id});
+        if(deletedUser?.deletedCount===0){
+            res.status(404).json({
+                message:"User not found"
+            });
+        } else {
+            res.status(200).json({
+                message:`User has been deleted with id: ${req.params.id}`
+            });
+        }
+    } catch (exception) {
         next(exception);
     }
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,15 +1,16 @@
 const express = require("express");
+const userController = require('../Controller/userController');
 const userRoute = express.Router();
 
-// userRoute.get("",userController.getAllUsers);
-// userRoute.post("/register",userController.addUser);
-// userRoute.get("/:id",userController.getUser);
+userRoute.get("",userController.getAllUsers);
+userRoute.post("/register",userController.addUser);
+userRoute.get("/:id",userController.getUser);
 // userRoute.get("/likes/:id", userController.getLikedTweets);
 // userRoute.get("/follow/:followingId/:followerId",userController.followUser);
 // userRoute.get("/unfollow/:followingId/:followerId",userController.unfollowUser);
 // userRoute.get("/followings/:id",userController.getFollowings);
 // userRoute.get("/followers/:id",userController.getFollowers);
-// userRoute.delete("/delete/:id",userController.deleteUser);
+userRoute.delete("/delete/:id",userController.deleteUser);
 userRoute.all("*",async(req, res)=>{
     console.log("Invalid path");
     res.status(404).json({
